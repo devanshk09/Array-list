eb5965f874d1b89729d5ea0993701806501f648e
devanshk09 <devanshkumar1004@gmail.com>
2025-02-03 23:45:08 +0530

    array to list

--- Main.java
+++ Main.java
@@ -10,4 +10,11 @@ public class Main {
         ArrayLists.separateEvenOdd(numbers);
         // Task 2: Find smallest distance between neighboring numbers
         System.out.println("Index of first number with smallest distance: " + ArrayLists.findSmallestDistance(numbers));
+        // Task 3: Convert array to ArrayList and back
+        ArrayList<Integer> list = ArrayLists.arrayToList(numbers);
+        System.out.println("Converted ArrayList: " + list);
+        int[] newArray = ArrayLists.listToArray(list);
+        System.out.println("Converted back to array: " + Arrays.toString(newArray));
+    }
+}
         
